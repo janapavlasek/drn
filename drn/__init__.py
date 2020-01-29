83109315fca75380230301b3173db670d6605c4e
Jana Pavlasek <jbpavlasek@gmail.com>
2020-01-28 21:07:21 -0500

    Install stats module

--- drn/__init__.py
+++ drn/__init__.py
@@ -2,6 +2,7 @@ from .log_util import create_logger
 from .io_util import save_checkpoint
 from . import segment
 from . import data_transforms
+from . import stats
 
 
-__all__ = ['create_logger', 'save_checkpoint', 'segment', 'data_transforms']
+__all__ = ['create_logger', 'save_checkpoint', 'segment', 'stats', 'data_transforms']
